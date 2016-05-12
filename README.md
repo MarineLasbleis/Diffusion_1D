47eb89d29b4f6424fee610ad3d029552ef0a6221
MarineLasbleis <marine.lasbleis@gmail.com>
2016-05-12 17:21:56 +0900

    README and main updated

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Diffusion_1D
 
 Test for diffusion in 1 dimension in a sphere
+
+-- diffusion_1D.py
+routines for solving diffusion.
+includes the tridiag solver, the Crack Nicholson scheme and the boundary conditions. 
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Time-stamp: <2015-07-23 11:41:42 marine>
+# Time-stamp: <2015-08-05 12:12:18 marine>
 
 
 import numpy as np
@@ -17,9 +17,16 @@ Nt=10 #number of timesteps that will be printed
 
 
 xAjouts=[1.,2.,3.]
-timeAjouts=[0.,0.1,0.2]
+timeAjouts=[0.,1.,2.]
 tempAjouts=[1.,2.,3.]
 Ajouts=dict(x=xAjouts, T=tempAjouts, time=timeAjouts)
+
+
+print "============ Main.py ============"
+print "=== Diffusion 1D cartesian ==="
+
+
+
 
 
 #Initial conditions : 
@@ -31,13 +38,13 @@ plt.plot(x,T,'o')
 
 
 
-def Solve_diffusion_cartesian(x,T,dx,dt,BC1,BC2,BCtype,kappa):
+def Solve_diffusion_cartesian(x,T,dx,dt,BC1,BC2,BCtype1,BCtype2,kappa):
 
     a,b,c,d = CrankNicholson_dr2_order2(T,dx,kappa)
     b = b+1/dt
     d = d+T/dt
-    d = BoundaryConditions(T,a,b,c,d,BC1,'bottom',typeBC=BCtype)
-    d = BoundaryConditions(T,a,b,c,d,BC2,'top',typeBC=BCtype)
+    d = BoundaryConditions(T,a,b,c,d,BC1,'bottom',typeBC=BCtype1)
+    d = BoundaryConditions(T,a,b,c,d,BC2,'top',typeBC=BCtype2)
     T = TDMAsolver(a, b, c, d)
     
     return T
@@ -55,30 +62,33 @@ dt=dx**2./kappa
 
 print dt, dx
 
+
 while time<Ajouts["time"][1]:
 
     if time+dt>Ajouts["time"][1]*compteur/Nt:
         print time+dt, dt, Ajouts["time"][1]*compteur/Nt
         dt=Ajouts["time"][1]*compteur/Nt-time
-        if dt<1e-12: dt=1e-12  
+        if dt<1e-5: dt=1e-5  
         time=Ajouts["time"][1]*compteur/Nt
         picture=1
         compteur=compteur+1
     else:
         picture=0
+        print "no picture", dt
         dt=dx**2./kappa
 
     time=time+dt
-    T=Solve_diffusion_cartesian(x,T,dx,dt,0.,0.,"Neumann",kappa,1.)
+    T=Solve_diffusion_cartesian(x,T,dx,dt,Ajouts["T"][0],0.,"Dir","Neumann",kappa)
 
     if picture:
-        print "picture", time, "CFL", kappa*dt/dx**2
+        print "picture", time, "CFL", kappa*dt/dx**2, "dt", dt
         plt.plot(x,T)
 
 x_2=np.linspace(Ajouts["x"][0]+dx/2.,Ajouts["x"][1],n)
 x=np.concatenate((x,x_2),axis=1)
 T=np.concatenate((T,Ajouts["T"][1]*np.ones(n)), axis=1)
 print time
+print "To", Ajouts["T"][0]
 plt.show()
 plt.plot(x,T,'o')
 
