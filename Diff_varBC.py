e998a9e5136064adb4b11b4ba6ca5436092a378f
MarineLasbleis <marine.lasbleis@gmail.com>
2016-05-18 14:41:57 +0900

    play with number of points. Now plot a (time-radius) evolution of temperature

--- Diff_varBC.py
+++ Diff_varBC.py
@@ -9,18 +9,14 @@ from diffusion_1D import *
 
 Nz = 50#number of space steps
 diffusivity = 1.
-Nt = 100 #number of time steps printed
-timeMax = 10.
-
-Nplots = 100 
+Nt = 20 #number of time steps printed
+timeMax = 20.
 
 zmax = 0.
 zmin = -10. #depth after wich we don't calculate
 
 BC_0 = {'type':'Dir', 'T0':1., 'position':0}
 BC_1 = {'type':'Dir', 'T0':0., 'position':-1}
-
-
 
 # Initial conditions
 z = np.linspace(zmin, zmax, Nz)
@@ -32,7 +28,7 @@ quantity[BC_1['position']] = BC_1['T0']
 
 plt.plot(z, quantity)
 
-quantity_stock = np.zeros((Nz, Nt))
+quantity_stock = np.zeros((Nz, Nt+1))
 quantity_stock[:,0] = quantity
 
 
@@ -43,32 +39,33 @@ dx = np.amin(np.diff(z))
 dt = 0.5*dx**2./diffusivity
 
 Niterations = (timeMax - time)/dt
-Ndiffplot = Niterations/Nplots
+Ndiffplot = Niterations/Nt
 print "test", Ndiffplot
 
 nn = 0
 while time < timeMax:
 
     nn += 1
-    print "iteration number: ", nn
-    print "time: ", time
     
-    TCMB = np.sin(time*2.*np.pi/9.)
+    TCMB = np.sin(time*2.*np.pi/9.)+2* np.sin(time*2.*np.pi/4.)+0.5*np.sin(time*2.*np.pi/0.5)+3*np.sin(time*2.*np.pi/20.)
     BC_0['T0']=TCMB
     # compute new quantity (temperature/composition) 
     new_quantity = Solve_diffusion_cartesian(z, quantity, dx, dt, BC_0, BC_1, diffusivity)
     time = time+dt
-    print nn%Ndiffplot
     if int(nn%Ndiffplot) == 0 :
+        print "plot number: ", int(nn/Ndiffplot)
+        print "time: ", time, ". Iteration number: ", nn
         plt.plot(z, new_quantity)
+        quantity_stock[:,int(nn/Ndiffplot)] = new_quantity
     quantity = new_quantity
 
 
 
-
-
-
-print dt, dx
+fig, ax = plt.subplots(2)
+time = np.linspace(0.,timeMax, Nt+1)
+ax[0].plot(time, np.sin(time*2.*np.pi/9.)+2* np.sin(time*2.*np.pi/4.)+
+             0.5*np.sin(time*2.*np.pi/0.5)+3*np.sin(time*2.*np.pi/20.))
+ax[1].matshow(quantity_stock)
 
 
 
